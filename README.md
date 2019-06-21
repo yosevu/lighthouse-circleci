d5f0f9586a0c42ff8bd9b0ba381f5a517baa116a
Yosevu Kilonzo <yosevuk@gmail.com>
2019-06-21 11:21:05 -0400

    fix the build

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # lighthouse-circleci
 
 An Example app with Lighthouse added to a CircleCI workflow.
+
+Notes
+
+- Workflow
+- Show failing score
+- Fix the build
+- Show deployed audit reason
+- Ways to show Lighthouse audit results (Circle, GitHub Checks, Comments, Repo)
--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-setTimeout(() => {
+// setTimeout(() => {
   ReactDOM.render(<App />, document.getElementById('root'));
-}, 3000);
+// }, 3000);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
